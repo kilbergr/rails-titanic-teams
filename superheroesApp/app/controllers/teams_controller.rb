1afc8caae36ab93d9fffef7aad47fac0e0022c8e
kilbergr <rekilberg@gmail.com>
2015-07-20 16:13:23 -0700

    more validations

--- superheroesApp/app/controllers/teams_controller.rb
+++ superheroesApp/app/controllers/teams_controller.rb
@@ -3,6 +3,7 @@ class TeamsController < ApplicationController
 	before_action :find_team, only: [:show, :edit, :update, :destroy]
 	before_action :find_user, only: [:index, :new, :create]
   before_action :confirm_logged_in, only: [:new, :edit, :destroy]
+  before_action :ensure_current_user, only: [:edit, :update, :destroy]
 
   
   def index
@@ -59,5 +60,15 @@ class TeamsController < ApplicationController
 
   def find_user
   	@user = User.find_by_id(params[:user_id])
+  end
+
+  def ensure_current_user
+    #find which user made the team with params[:id]
+    team = Team.find_by_id params[:id]
+    unless team.user_id == session[:user_id]
+    #compare against current_user.id, redirect if doesn't match
+      redirect_to root_path, alert: "You cannot alter other users' teams" 
+    end
+    
   end
 end
--- superheroesApp/app/controllers/users_controller.rb
+++ superheroesApp/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 
 class UsersController < ActionController::Base
 
-	
+	def ensure_correct_user
+		user = User.find_by_id params[:id]
+		unless user.id == session[:user_id]
+			redirect_to root_path, alert: "You cannot alter other users' information"
+		end
+	end
 
 end
